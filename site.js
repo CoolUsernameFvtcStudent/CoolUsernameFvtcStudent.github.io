0a6cb04ecee8131dd3a1efa56216d09e8b8a878e
CoolUsernameFvtcStudent <500192037fvtc@gmail.com>
2025-03-30 04:47:01 -0500

    two

--- site.js
+++ site.js
@@ -1,8 +1,21 @@
+const we = document.querySelector("#welcome")
+
 const hours = new Date().getHours() // get the current hour
 
 const isMorning = hours >= 4 && hours < 12 // is it morning?
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
 
+//we.textContent
 
-console.log("https://fvtc.software/appel/modern-javascript/assignments/homepage");
+if (isMorning) {
+    we.textContent = "Good morning!";
+} else if (isAfternoon) {
+    we.textContent = "Good afternoon!";
+} else if (isEvening) {
+    we.textContent = "Good evening!";
+}
+
+
+
+console.log("https://fvtc.software/appel/modern-javascript/assignments/homepage"); 
