cb40e61d31d4ce2e62e0f6820e19841a831d1cc9
CoolUsernameFvtcStudent <500192037fvtc@gmail.com>
2025-05-13 11:19:18 -0500

    Added carousel

--- site.js
+++ site.js
@@ -1,4 +1,26 @@
 const we = document.querySelector("#welcome")
+const previous = document.getElementById('prev')
+const next = document.getElementById('next')
+
+
+previous.addEventListener('click', () => {
+    currentImage = currentImage - 1
+    //
+    if (currentImage < 0) {currentImage = 4}
+    //
+    showImages()
+    //
+});
+next.addEventListener('click', () => {
+    currentImage = currentImage + 1
+    //
+    if (currentImage > 4) {currentImage = 0}
+    //
+    showImages()
+    //
+});
+
+
 
 const hours = new Date().getHours() // get the current hour
 
@@ -17,3 +39,34 @@ if (isMorning) {
 }
 
 localStorage.setItem("It's a secret to everybody.", "super cool secret");
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+setInterval(() => {
+    currentImage = currentImage + 1
+    //
+    if (currentImage > 4) {currentImage = 0}
+    //
+    showImages()
+    //
+}, 5000)
+
